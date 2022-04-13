d9b3abbd8bee68163a67c84bcfd8e5bbf7b80e57
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-13 14:21:41 +0200

    first upload

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+    <div class="counter">
+        {{counter}}
+      </div>
+      <button  @click="decreaseCounter">-</button>
+     <button @click="increaseCounter">+</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+data(){
+  return{
+    counter:0
+  }
+},
+methods:{
+  increaseCounter(){
+    this.counter++
+  },
+  decreaseCounter(){
+    this.counter--
+  },
+}
+
+}
+</script>
+<style scoped>
+div{
+  margin:10px;
+}
+.counter{
+  font-size:80px;
+}
+button{
+  font-size:40px;
+  width:100px ;
+  margin:0 10px;
+}
+</style>
